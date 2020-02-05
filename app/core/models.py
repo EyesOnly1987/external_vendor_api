b5e06023d37524f927a7facf04e6088d0273e0b6
EyesOnly1987 <53872441+EyesOnly1987@users.noreply.github.com>
2020-02-05 11:45:56 -0700

    updated amin to support custoom user  Please enter

--- app/core/models.py
+++ app/core/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new User"""
+        """Creates and saves a new User normalize_email is a helper function"""
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
